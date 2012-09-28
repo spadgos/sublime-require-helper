953107ee5b7aff2f64c477ed9c9f65592cd3988e
Nick Fisher <spadgos@gmail.com>
2012-09-28 17:18:39 +0200

    Ctrl+K, Ctrl+R will go to the module under the cursor

--- require_helper.py
+++ require_helper.py
@@ -44,7 +44,7 @@ class RequireHelperCommand(sublime_plugin.TextCommand):
         varName = string.capwords(('/' + include).rsplit('/', 1)[1], '-').replace('-', '')
 
         #  align with adjacent requires
-        lineCounter = -1
+        lineCounter = 1
         numSpaces = 1
         while True:
             line = readLine(self.view, insertPos, lineCounter)
@@ -60,15 +60,16 @@ class RequireHelperCommand(sublime_plugin.TextCommand):
                         lineCounter += 1 if lineCounter > 0 else -1
                         continue
 
-            if lineCounter < 0:
+            if lineCounter > 0:
                 lineCounter *= -1
             else:
                 break
 
-        return "%s%s= require('%s')" % (
+        return "%s%s= require('%s')%s" % (
             varName,
             " " * numSpaces,
-            include
+            include,
+            "," if lineCounter > 0 else ";"
         )
 
     def quick_panel(self, *args, **kwargs):
@@ -105,3 +106,60 @@ def getFiles(relativePath, base, files):
     except OSError:
         print "RequireHelper: could not find " + path
         return []
+
+
+class RequireHelperGotoDef(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        v = self.view
+        window = sublime.active_window()
+        wordRegion = v.word(v.sel()[0].begin())
+        posBefore = wordRegion.begin() - 1
+
+       # func = None
+        req = v.substr(wordRegion)
+
+        if posBefore >= 0:
+            if v.substr(posBefore) == '.':
+                # func = req
+                req = v.substr(v.word(posBefore))
+
+        if not req:
+            return
+
+        requires = self.findRequires()
+        require = requires[req]
+        if not require:
+            return
+
+        base = self.view.settings().get('require_helper_base') or ""
+        folders = window.folders()
+
+        for folder in folders:
+            path = os.path.join(folder, base, require)
+
+            if not os.path.exists(path):
+                path += '.js'
+
+            if os.path.exists(path):
+                window.open_file(path)
+                break
+        # todo: navigate to the function
+
+    def findRequires(self):
+        v = self.view
+        nameToken = '[a-z_$][a-z_$0-9]*'
+        path = '(?:/?[a-z0-9_$\\-\\s]+)+'  # TODO: there's a lot more valid path names
+        strs = []
+        v.find_all('(%s)\\s*=\\s*require\\(["\'](%s)["\']\\)' % (nameToken, path),
+            sublime.IGNORECASE,
+            '$1|||$2',
+            strs
+        )
+
+        # there is totally a more python-y way to do this...
+        pairs = {}
+        for line in strs:
+            split = line.split('|||')
+            pairs[split[0]] = split[1]
+        return pairs
